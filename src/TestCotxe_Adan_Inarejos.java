5f0d3194dbadc2e0072c305ecb9667316a74a92c
ainarejos <ainarejos@nigul.cide.es>
2020-12-09 17:32:27 +0100

    Version Final.

--- src/TestCotxe_Adan_Inarejos.java
+++ src/TestCotxe_Adan_Inarejos.java
@@ -1,7 +1,7 @@
 public class TestCotxe_Adan_Inarejos {
     public static void main(String[] args){
         try {
-            Coche_Ismael_Martin c1= new Coche_Ismael_Martin("Audi", "r8", TipusCanvi.CanviManual);
+            Cotxe_Adan_Inarejos c1= new Cotxe_Adan_Inarejos("Audi", "r8", TipusCanvi.CanviManual);
             c1.arrancarMotor();
             System.out.println(c1.comprovaMotor());
             System.out.println(c1.getRevolucions());
