bb9fe030d4244ccdf28baa8d4cff5b25d3323e29
ainarejos <ainarejos2@nigul.cide.es>
2020-12-16 15:19:53 +0100

    Se han añadido comentarios.

--- src/CotxeSegonaPart_Adan_Inarejos.java
+++ src/CotxeSegonaPart_Adan_Inarejos.java
@@ -16,6 +16,7 @@ enum MarxasManual {
 }
 
 public class CotxeSegonaPart_Adan_Inarejos extends Cotxe_Adan_Inarejos{
+    //Atributos.
     private EstatsMotorCotxe estado=EstatsMotorCotxe.Aturat;
     private int revoluciones=0;
     private MarxasAutomatic marxasA=MarxasAutomatic.N;
@@ -24,8 +25,12 @@ public class CotxeSegonaPart_Adan_Inarejos extends Cotxe_Adan_Inarejos{
     public CotxeSegonaPart_Adan_Inarejos(String marca, String model, TipusCanvi tipuscanvi) {
         super(marca, model, tipuscanvi);
     }
+    //Creamos dos metodos, uno para cambiar las marchas de un coche automatico, y otro para un coche manual.
+    //Metodo para el coche automatico.
     public void CanviarMarxaAutomatic(char a) throws Exception {
+        //Creamos una condicion que unicamente permita cambiar de marxa si el coche es automatico y esta en marxa.
         if (tipuscanvi.equals(TipusCanvi.CanviAutomatic)&&comprovaMotor().equals(EstatsMotorCotxe.EnMarxa)){
+            //Se crean la condicion + y -, y en el caso de que se introduzca otro caracter lanze una excepcion.
             if (a=='+'){
                 switch (marxasA){
                     case R:
@@ -55,15 +60,18 @@ public class CotxeSegonaPart_Adan_Inarejos extends Cotxe_Adan_Inarejos{
             } else {
                 throw new Exception("Los unicos valores validos son + o -.");
             }
+            //Creamos dos posibles excepciones, una en el caso de que el coche no sea automaticio, y otra para el caso de que el coche este apagao.
         } else if (comprovaMotor().equals(EstatsMotorCotxe.EnMarxa)){
             throw new Exception("Este coche es manual, por lo tanto no puedes usar el metodo de marchas automaticas.");
         } else {
             throw new Exception("El coxhe esta apagado, por lo tanto no puedes cambiar de marxa");
         }
     }
-
+    //Metodo para el coche manual.
     public void CamviarMarxaManual(char a) throws Exception {
+        //Creamos una condicion que unicamente permita cambiar de marxa si el coche es manual y esta en marxa.
         if (tipuscanvi.equals(TipusCanvi.CanviManual)&&comprovaMotor().equals(EstatsMotorCotxe.EnMarxa)){
+            //Se crean la condicion + y -, y en el caso de que se introduzca otro caracter lanze una excepcion.
             if (a=='+'){
                 switch (marxasM){
                     case R:
@@ -132,22 +140,18 @@ public class CotxeSegonaPart_Adan_Inarejos extends Cotxe_Adan_Inarejos{
             } else {
                 throw new Exception("Solo puedes introducir los valores + o -.");
             }
+            //Creamos dos posibles excepciones, una en el caso de que el coche no sea manual, y otra para el caso de que el coche este apagao.
         } else if (comprovaMotor().equals(EstatsMotorCotxe.EnMarxa)) {
             throw new Exception("Este coche es Automatico, por lo tanto no puedes usar el metodo de marchas Manuales.\"");
         } else {
             throw new Exception("El coche esta apagado, por lo tanto no puedes cambiar de marxa");
         }
     }
-
+    // Getters para mostrar las marxas.
     public MarxasAutomatic getMarxasA() {
         return marxasA;
     }
-
     public MarxasManual getMarxasM() {
         return marxasM;
-    }
-
-    public EstatsMotorCotxe getEstado() {
-        return estado;
     }
 }
